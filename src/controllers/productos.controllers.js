95355bfc520ff3be3cbaea0a9745c134355e61e0
Santiago <concursos.utn.frt@gmail.com>
2025-08-13 20:19:14 -0300

    podemos ver los datos que a la solicitud a la base de datos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -8,7 +8,15 @@ export const leerProductos = (req, res)=>{
 }
 //agregar funcion para crear producto
 export const crearProducto =(req, res)=>{
-    
+    try{
+        //1- recibir el objeto que tengo que agregar a la BD
+        console.log(req.body)
+        //2- validar los datos del objetos
+        //3- guardar el objeto en la base de datos
+        //4- enviar respuesta
+    } catch (error){
+        console.error(error)
+    }
 }
 
 //agregar funcion para editar producto
