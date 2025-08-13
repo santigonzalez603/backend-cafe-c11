08c381cf504358e83783acaa0d1f6dca71782f62
Santiago <concursos.utn.frt@gmail.com>
2025-08-12 21:06:33 -0300

    realize la configuracion del server

---
+++ index.js
@@ -0,0 +1,8 @@
+import Server from "./server/config.js";
+
+const server = new Server();
+
+//agregar las rutas
+
+server.listen()
+
--- server/config.js
+++ server/config.js
@@ -1,5 +1,32 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
 export default class Server{
     constructor(){
-        
+        this.app = express()
+        this.port = process.env.PORT || 3000
+        this.middlewares()
+    }
+
+    //agregar metodos
+
+    //middlewares
+    middlewares(){
+        this.app.use(cors()) //permite tener conexiones remotas
+        this.app.use(express.json()) //interpreta los datos json que llegan en la solicitud
+        this.app.use(morgan('dev'))
+        const __dirname = dirname(fileURLToPath(import.meta.url))
+        this.app.use(express.static(`${__dirname}/../public`))
+        console.log(`${__dirname}/../public`)
+    }
+
+    //escuchar el puerto
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.info(`El servidor se esta ejecutando en: http://localhost:${this.port}`)
+        })
     }
 }
