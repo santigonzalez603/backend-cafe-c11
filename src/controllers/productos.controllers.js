30f443001a2d0ff54185c7c553aecb22e456b876
Santiago <concursos.utn.frt@gmail.com>
2025-08-31 14:12:22 -0300

    agregamos la funcion de leerProductos para devolver toda la coleccion de productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -6,8 +6,16 @@ export const test = (req, res)=>{
     res.stand('Primera prueba desde el cmd')
 }
 
-export const leerProductos = (req, res)=>{
-
+export const leerProductos = async (req, res)=>{
+    try{
+        //1- buscar todos los productos en la base de datos
+        const listaProductos = await Producto.find()
+        //2. enviar la respuesta al front
+        res.status(200).json(listaProductos)
+    }catch (error){
+        console.error(error);
+        res.status(500).json({mensaje: 'Error al leer los producto'})
+    }
 }
 //agregar funcion para crear producto
 export const crearProducto = async (req, res)=>{
